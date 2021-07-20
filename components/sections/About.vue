a0604f94c4e945bba1d955ba615e4ab8aaa0c5b3
jancarlohendriks <jancarlohendriks@hotmail.com>
2021-07-20 22:37:35 +0200

    minor changes

--- components/sections/About.vue
+++ components/sections/About.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="row">
     <div class="col col-xl-10">
-      <h2 class="section-title animation-translate-overline animation-item-1">
-        {{ content.title }}
+      <h2
+        class="section-title animation-translate-overline animation-item-1"
+        v-if="content.title"
+      >
+        {{ content.title }}>
       </h2>
       <article class="article animation-translate animation-item-2">
-				<nuxt-content :document="content" />
+        <nuxt-content :document="content" />
       </article>
     </div>
   </div>
 </template>
 <script>
 export default {
-	data() {
-		return {
-			content: []
-		}
-	},
-	async created() {
-		this.content = await this.$content('', 'about').fetch()
-	}
+  data() {
+  	return {
+  		content: []
+  	}
+  },
+  // async asyncData({ $content, params }) {
+  //   content = await this.$content('about').fetch()
+  //   return { content }
+  // },
+  async created() {
+  	this.content = await this.$content('about').fetch()
+  }
 }
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,11 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
 	// ssr: false,
+	// mode:'universal',
 	loading: false,
-
-	mode:'universal',
 
 	generate: {
 		routes: [
-			'/about',
 			'/bert-govers',
 			'/mooi',
 			'/riverco',
@@ -15,6 +13,21 @@ export default {
 			'/strp',
 		]
 	},
+
+	// generate: {
+  //   async routes () {
+  //     // next comment to make VSCode ignore the "error"
+  //     // @ts-ignore
+  //     const { $content } = require('@nuxt/content')
+  //     const pages = await $content().only(['path']).fetch()
+  //     // const posts = await $content('posts').only(['path']).fetch()
+      
+  //     // const files = [...pages,...posts,]
+  //     const files = [...pages]
+
+  //     return files.map(file => file.path === '/index' ? '/' : file.path)
+  //   }
+  // },
 
   head: {
     title: 'portfolio-2021',
